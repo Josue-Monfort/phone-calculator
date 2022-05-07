029758eb75c703b755a9dc1f36d50971c9645164
josue <josuemonfortsantiago@gmail.com>
2022-05-07 18:51:51 -0300

    Added the basic math operations functions and the calculate function

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 
+// Takes a math operator and two numbers 
+// and then it does the math calculation on those numbers
+function calculate(operator, num1, num2) {
+    num1 = parseFloat(num1); // To make sure the input is always numbers.
+    num2 = parseFloat(num2);
+    switch (operator) {
+        case "+":
+            return addNumber(num1, num2);
+        case "-":
+            return subtractNumber(num1, num2);
+        case "*":
+            return multiplyNumber(num1, num2);
+        case "÷":
+            return divideNumber(num1, num2);
+        case "%":
+            return getPorcentage(num1, num2);
+    };
+};
 
+function addNumber(num1, num2) {
+    return num1 + num2;
+};
+
+function subtractNumber(num1, num2) {
+    return num1 - num2;
+};
+
+function multiplyNumber(num1, num2) {
+    return num1 * num2;
+};
+
+function divideNumber(num1, num2) {
+    return num1 / num2;
+};
+
+function getPorcentage(num1, num2) {
+    return ((num1 / 100) * num2);
+};
 
 // Show app function
 const userHourDisplay = document.querySelector(".hourDisplay");
